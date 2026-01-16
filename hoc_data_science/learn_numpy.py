8ddb2677d21ecad46bf671cf5a6f5375fecb8a69
minh hung <minhhung191205@gmail.com>
2026-01-16 23:31:25 +0700

    numpy

--- hoc_data_science/learn_numpy.py
+++ hoc_data_science/learn_numpy.py
@@ -2,3 +2,69 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+np.array([1,2,3,4])
+
+np.array([3.14,4,5,6])
+
+#convert sang kiểu float
+np.array([1,2,3,4] , dtype='float32')
+
+a2 = np.array([[1,2,3],[3,4,5]])
+type(a2)
+
+# hiển thị số hàng và số cột
+a2.shape
+
+# hiển thị số chiều của mảng
+a2.ndim
+
+a2.size
+
+#Create numpy array from hàm có sẳn (zeros , ones , full , arange , linspace)
+np.zeros([2,4] ,  dtype=int)
+
+np.ones([3,5] , dtype=float)
+
+#function arange (start , end , step)
+np.arange(0 , 20, 2)
+
+np.full((3,5) , 6.9)
+
+np.linspace(0, 1 , 5)
+
+#Random
+# nếu mình muốn random ở đâu cx có giá trị giống nhau khi random như vậy thì sử dụng seed()
+
+np.random.seed(34)
+
+np.random.random((4,4))
+
+np.random.normal(0,2,(3,3))
+
+x2 = np.random.randint(0,10, size=6)
+
+# slicing array
+x1 = np.random.randint(20, size=6)
+x1
+
+x1[1:4]
+
+# reshape & transpose
+grid = np.arange(1,10)
+grid.shape
+
+grid.reshape((3,3))
+
+
+np.vstack((x1,x2))
+np.hstack([x1,x2])
+
+#splitting of array
+x = np.array([1,2,3,99,69,3,2,1])
+np.split(x , [3,5])
+
+#tính giá trị trung bình
+np.mean()
+
+#độ lệch chuẩn
+np.std()
