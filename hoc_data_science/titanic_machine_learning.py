2f96be0b28d8330c4670af7487726f6103963ea0
minh hung <minhhung191205@gmail.com>
2026-01-27 11:29:49 +0700

    train baseline models

--- hoc_data_science/titanic_machine_learning.py
+++ hoc_data_science/titanic_machine_learning.py
@@ -155,3 +155,98 @@ X_test = preprocessor.transform(X_test)
 from sklearn.model_selection import train_test_split
 
 X_train , X_val , y_train , y_val = train_test_split(X , y , test_size=0.2)
+
+# Binary classification
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import precision_score , recall_score , classification_report , confusion_matrix
+
+log_reg = LogisticRegression(solver='liblinear' , max_iter=1000)
+log_reg.fit(X_train , y_train)
+
+log_reg.score(X_val, y_val)
+y_pred = log_reg.predict(X_val)
+precision_score(y_val , y_pred) , recall_score(y_val , y_pred)
+
+print(classification_report(y_val, y_pred))
+
+# Nâng bậc của linear y = ax1 + bx2 + bias -> y = ax1^2 + bx2^2 + cx1*x2 + bias sử dụng PolynomiaFeatures
+poly = PolynomialFeatures(degree = 2)
+poly_features_X_train = poly.fit_transform(X_train)
+poly_features_X_val = poly.transform(X_val)
+
+poly_log_reg = LogisticRegression(solver='liblinear' , max_iter=1000)
+poly_log_reg.fit(poly_features_X_train , y_train)
+
+poly_log_reg.score(poly_features_X_val , y_val)
+
+#Decision Tree
+decision_tree = DecisionTreeClassifier(criterion='entropy' , max_depth=8 , random_state= 2026)
+decision_tree.fit(X_train , y_train)
+
+decision_tree.score(X_val , y_val)
+
+# Cross-Vadication
+from sklearn.model_selection import cross_val_score
+log_reg_cv = LogisticRegression(solver='liblinear' , max_iter=1000)
+dt_cv = DecisionTreeClassifier(criterion='entropy' , max_depth=8 , random_state= 2026)
+
+lr_scores = cross_val_score(log_reg_cv , X , y , scoring = 'accuracy' , cv = 5)
+lr_scores.mean() , lr_scores.std()
+
+dt_scores = cross_val_score(dt_cv , X , y , scoring = 'accuracy' , cv = 5)
+dt_scores.mean() , dt_scores.std()
+
+# Base line Model Comparison
+
+from sklearn.svm import LinearSVC , SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier , GradientBoostingClassifier , ExtraTreesClassifier , AdaBoostClassifier
+from xgboost import XGBClassifier
+seed = 2026
+
+models = [
+    LinearSVC(max_iter=12000,random_state=seed),
+    SVC(random_state=seed),
+    KNeighborsClassifier(metric='minkowski' , p=2),
+    LogisticRegression(solver='liblinear' , max_iter=1000),
+    DecisionTreeClassifier(criterion='entropy' ,random_state= 2026),
+    RandomForestClassifier(random_state=seed),
+    GradientBoostingClassifier(random_state=seed),
+    ExtraTreesClassifier(),
+    AdaBoostClassifier(),
+    XGBClassifier(use_label_encoder = False , eval_metric='logloss' ,random_state=seed)
+]
+
+from sklearn.model_selection import StratifiedKFold
+def generate_baseline_results(models , X, y , metrics , cv =5 , plot_results = False):
+  #define k-fold:
+  kflod = StratifiedKFold(cv , shuffle=True , random_state=seed)
+  entries = []
+  for model in models:
+    model_name = model.__class__.__name__
+    scores = cross_val_score(model , X , y , scoring = metrics , cv = kflod)
+    for flod_idx , score in enumerate(scores):
+      entries.append((model_name , flod_idx , score))
+
+  cv_df = pd.DataFrame(entries , columns=['model_name' , 'fold_idx' , 'accuracy_score'])
+
+  if plot_results:
+    sns.boxplot(x='model_name' , y='accuracy_score' , data=cv_df , color='skyblue' , showmeans= True)
+    plt.title("Boxplot of Baseline Model Accuracy using 5-fold cross-validation")
+    plt.xticks(rotation=45)
+    plt.show()
+
+  #Summary result
+  mean = cv_df.groupby('model_name')['accuracy_score'].mean()
+  std = cv_df.groupby('model_name')['accuracy_score'].std()
+
+  baseline_results = pd.concat([mean , std] , axis=1 , ignore_index=True)
+  baseline_results.columns = ['Mean' , 'Standard Deviation']
+
+  baseline_results.sort_values(by=['Mean'] , ascending=False , inplace=True)
+
+  return baseline_results
+
+generate_baseline_results(models , X , y , metrics = 'accuracy' , cv=5 , plot_results= False)
